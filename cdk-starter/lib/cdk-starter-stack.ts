17048fc117e628af6fa3e383adea9d33760672bf
matfij <mateusz.michal.fijak@gmail.com>
2023-09-25 21:39:26 +0200

    define base constructs

--- cdk-starter/lib/cdk-starter-stack.ts
+++ cdk-starter/lib/cdk-starter-stack.ts
@@ -1,16 +1,47 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStarterStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // L1 construct - deployment parameter
+        const duration = new cdk.CfnParameter(this, 'duration', {
+            type: 'Number',
+            default: 1,
+            minValue: 1,
+            maxValue: 7,
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStarterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // L1 construct
+        new CfnBucket(this, 'cdk-bucket-l1', {
+            lifecycleConfiguration: {
+                rules: [{ expirationInDays: duration.valueAsNumber, status: 'Enabled' }],
+            },
+        });
+
+        // L2 construct
+        const l2Bucket = new Bucket(this, 'cdk-bucket-l2', {
+            lifecycleRules: [{ expiration: cdk.Duration.days(duration.valueAsNumber) }],
+        });
+
+        // L3 construct
+        class L3Bucket extends Construct {
+            constructor(scope: Construct, id: string, expirationHours = duration.valueAsNumber) {
+                super(scope, id);
+
+                new Bucket(this, 'cdk-bucket-l3-inner', {
+                    lifecycleRules: [{ expiration: cdk.Duration.days(expirationHours) }],
+                });
+            }
+        }
+        new L3Bucket(this, 'cdk-bucket-l3', 48);
+
+        // L1 construct for inter-stack data exchange
+        new cdk.CfnOutput(this, 'L2 Bucket Name', {
+            value: l2Bucket.bucketName,
+            exportName: 'L2BucketName',
+        });
+    }
 }
--- notes/1-couldformation.md
+++ notes/1-couldformation.md
@@ -1,4 +1,4 @@
-## CloudFormation
+## CloudFormation (Cfn)
 
 Infrastructure as a code soultion - organizes AWS resources into Stacks.
 Based on JSON/YAML config it creates AWS resources. (Only L1 resources)
@@ -18,3 +18,15 @@ Base building blocks of AWS infrastructure resources, there are 3 level of const
 1. Low level constructs (L1) - Cloud formation resources, requires long configuration
 2. AWS resources - CDK defaults, boilerplate, type safety
 3. Patterns - combine multiple type of resources (Serverless API = Lambda + Gateway)
+
+---
+
+## AWS CDK commands
+
+-   `cdk init app ==language typescript`
+-   `cdk bootstrap` (prepares environment for all applications)
+-   `cdk synth` (validates and generates Cfn template)
+-   `cdk deploy` (deploys CDK stack, optionally with parameters: `--parameters duration=3`)
+-   `cdk list` (prints all stacks)
+-   `cdk diff` (local vs deployed version)
+-   `cdk destroy <stack_name>` (deletes deployed stack)
