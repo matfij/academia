41b4dce794bfbb6b15621ef3f0ab0b7292bf9954
matfij <mateusz.michal.fijak@gmail.com>
2023-12-01 21:27:54 +0100

    setup data seeder

--- php-laravel/drill-queue/README.md
+++ php-laravel/drill-queue/README.md
@@ -2,3 +2,7 @@
 
 -   start server: `php artisan serve`
 -   list routes: `php artisan route:list`
+-   apply migrations: `php artisan migrate`
+-   rollback migrations: `php artisan migrate:rollback`
+-   seed dummy data into db: `php artisan db:seed`
+-   reset db state and seed: `php artisan migrate:refresh --seed`
---
+++ php-laravel/drill-queue/database/factories/DrillFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Drill>
+ */
+class DrillFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence,
+            'description' => fake()->paragraph(4),
+            'hints' => fake()->paragraph(1),
+            'completed' => fake()->boolean,
+        ];
+    }
+}
