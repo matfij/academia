686014b52bc736a1d55a0ff6d648652b964d3d50
matfij <mateusz.michal.fijak@gmail.com>
2023-12-17 21:08:36 +0100

    implement book queries

--- php-laravel/book-reviews/app/Models/Book.php
+++ php-laravel/book-reviews/app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +13,40 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, string $from = null, string $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn (Builder $q) => $this->dateRangeBuilder($q, $from, $to)
+        ])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, string $from = null, string $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn (Builder $q) => $this->dateRangeBuilder($q, $from, $to)
+        ], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeBuilder(Builder $query, string $from = null, string $to = null)
+    {
+        if ($from && ! $to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
