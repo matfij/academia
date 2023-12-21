c9aba23d0ed42227abb1769a68e4b5e27f2d6159
matfij <mateusz.michal.fijak@gmail.com>
2023-12-21 21:05:36 +0100

    implement books filtering view

--- php-laravel/book-reviews/resources/views/books/index.blade.php
+++ php-laravel/book-reviews/resources/views/books/index.blade.php
@@ -3,10 +3,26 @@
     <section>
         <h1 class="mb-10 text-2xl">Book Lib</h1>
         <form method="GET" action="{{ route('books.index') }}" class="flex gap-2 mb-4">
+            <input type="hidden" name="filter" value="{{ request('filter') }}">
             <input name="title" value="{{ request('title') }}" type="text" placeholder="Search by title" class="input" />
             <button type="submit" class="btn">Search</button>
             <a href="{{ route('books.index') }}" class="btn">Clear</a>
         </form>
+        <div class="filter-container flex mb-4">
+            @php
+                $filters = [
+                    '' => 'Latest',
+                    'trending' => 'Trending',
+                    'highest_rated' => 'Highest Rated',
+                ];
+            @endphp
+            @foreach ($filters as $filter => $label)
+                <a href="{{ route('books.index', [...request()->query(), 'filter' => $filter]) }}"
+                    class="{{ request('filter') === $filter || (request('filter') === null && $filter === '') ? 'filter-item-active' : 'filter-item' }}">
+                    {{ $label }}
+                </a>
+            @endforeach
+        </div>
         <ul>
             @forelse ($books as $book)
                 <li class="mb-4 book-item flex flex-wrap items-center justify-between">
