0540960e81e91c10206a9cb17c50456307230fab
matfij <mateusz.michal.fijak@gmail.com>
2024-02-06 20:45:52 +0100

    implement login form

--- react-blueprint/src/pages/login/LoginComponent.tsx
+++ react-blueprint/src/pages/login/LoginComponent.tsx
@@ -1,11 +1,42 @@
 import { useNavigate } from 'react-router-dom';
 import { UsersClient } from '../../common/api/client';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { UserSignupDto } from '../../common/api/generated';
 
 export const LoginComponent = () => {
+    const [formData, setFormData] = useState<UserSignupDto>({
+        login: '',
+        password: '',
+    });
+    const [formErrors, setFormErrors] = useState<Partial<UserSignupDto>>();
     const navigate = useNavigate();
 
-    const onLogin = async () => {
-        const res = await UsersClient.signin({ body: { login: 'test', password: 'test' } });
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const checkFormErrors = () => {
+        const errors: Partial<UserSignupDto> = {};
+        let errorOccured = false;
+        if (!formData.login) {
+            errors.login = 'errors.loginRequired';
+            errorOccured = true;
+        }
+        if (!formData.password) {
+            errors.password = 'errors.passwordRequired';
+            errorOccured = true;
+        }
+        setFormErrors(errors);
+        return errorOccured;
+    };
+
+    const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (checkFormErrors()) {
+            return;
+        }
+        const res = await UsersClient.signin({ body: formData });
         console.log(res.accessToken, res.login, res.level);
         navigate('home');
     };
@@ -13,7 +44,31 @@ export const LoginComponent = () => {
     return (
         <>
             <h2>Login Component</h2>
-            <button onClick={() => onLogin()}>Login</button>
+            <form onSubmit={handleLogin}>
+                <label htmlFor="login">
+                    Login:
+                    <input
+                        type="text"
+                        name="login"
+                        id="login"
+                        value={formData.login}
+                        onChange={handleInputChange}
+                    />
+                    {formErrors?.login}
+                </label>
+                <label htmlFor="password">
+                    Password:
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                {formErrors?.password}
+                <button type="submit">Login</button>
+            </form>
         </>
     );
 };
