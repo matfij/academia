0cd79e0d84cad7c210f650624130cc8fc1619d89
matfij <mateusz.michal.fijak@gmail.com>
2024-04-19 20:29:02 +0200

    add products page skeleton

---
+++ nextjs-14/src/app/admin/products/page.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import { Button } from '../../../shared/components/shadcn/button';
+import PageHeaderComponent from '../_components/page-header';
+import { Table, TableBody, TableHead, TableHeader, TableRow } from '../../../shared/components/shadcn/table';
+
+export default function AdminProductsPage() {
+    return (
+        <>
+            <div className="flex justify-between items-center gap-4">
+                <PageHeaderComponent>Products</PageHeaderComponent>
+                <Button>
+                    <Link href="admin/products/new">Add Product</Link>
+                </Button>
+            </div>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-0">
+                            <span className="sr-only">Available for purchase</span>
+                        </TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Price</TableHead>
+                        <TableHead>Orders</TableHead>
+                        <TableHead className="w-0">
+                            <span className="sr-only">Actions</span>
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    ...
+                </TableBody>
+            </Table>
+        </>
+    );
+}
