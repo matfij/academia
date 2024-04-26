659a8d6581f8e4a9ac16da9d7de5a6a76618ba5a
matfij <mateusz.michal.fijak@gmail.com>
2024-04-26 19:45:29 +0200

    add admin auth middleware

--- nextjs-14/src/shared/lib/utils.ts
+++ nextjs-14/src/shared/lib/utils.ts
@@ -12,3 +12,18 @@ export function wait(durationMs: number) {
 export function getImagePath(path: string) {
     return `${path.replace('public', '')}`;
 }
+
+export function getEnvVar<T extends string | number | boolean>(name: string): T {
+    return process.env[name] as T;
+}
+
+export async function hashPassword(password: string) {
+    const arrBuff = await crypto.subtle.digest('SHA-512', new TextEncoder().encode(password));
+    return Buffer.from(arrBuff).toString('base64');
+}
+
+export async function checkPassword(password: string, hash: string) {
+    const hashedPassword = await hashPassword(password);
+    console.log(hashedPassword);
+    return hashedPassword === hash;
+}
