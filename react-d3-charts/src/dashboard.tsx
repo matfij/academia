6f315a6fb516594bc60c5f38d7dab96275c47ea0
matfij <mateusz.michal.fijak@gmail.com>
2025-01-12 17:29:10 +0100

    Add chart data extending

--- react-d3-charts/src/dashboard.tsx
+++ react-d3-charts/src/dashboard.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { LinearChart, LinearChartProps } from './charts/linear-chart';
 
 export const Dashboard = () => {
-    const [lineChartData] = useState<LinearChartProps>({
+    const [lineChartData, setLineChartData] = useState<LinearChartProps>({
         title: 'Linear Data',
         xLabel: 'time [s]',
         yLabel: 'acceleration [m/s^]',
@@ -10,10 +10,20 @@ export const Dashboard = () => {
         y: Array.from({ length: 250 }, (_, i) => i + 2 * Math.sin(i)),
     });
 
+    const extendLineChartData = () => {
+        setLineChartData((prev) => ({
+            ...prev,
+            x: [...prev.x, prev.x[prev.x.length - 1] + 1],
+            y: [...prev.y, prev.x[prev.y.length - 1] + 2 * Math.sin(prev.y[prev.y.length - 1])],
+        }));
+        console.log(lineChartData.y[lineChartData.y.length - 1]);
+    };
+
     return (
         <div className="dashboardWrapper">
             <h1 style={{ margin: '1rem' }}>Dashboard</h1>
             <LinearChart {...lineChartData} />
+            <button onClick={extendLineChartData}>Extend</button>
         </div>
     );
 };
