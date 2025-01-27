eccab6aa8522727aaac1dc7af783cf651ea6cb88
matfij <mateusz.michal.fijak@gmail.com>
2025-01-27 20:47:27 +0100

    Native capabilities

--- MauiBlazor/MauiBlazor/MainPage.xaml.cs
+++ MauiBlazor/MauiBlazor/MainPage.xaml.cs
@@ -6,5 +6,10 @@
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Native Aler", "", "Cancel");
+        }
     }
 }
