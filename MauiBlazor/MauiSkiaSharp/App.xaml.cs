beaba677d30ae156c00d9096fb8da1dde00d1785
matfij <mateusz.michal.fijak@gmail.com>
2025-01-30 20:47:48 +0100

    Setup skia sharp

---
+++ MauiBlazor/MauiSkiaSharp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiSkiaSharp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
