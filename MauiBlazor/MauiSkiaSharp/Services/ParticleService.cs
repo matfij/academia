e78a1878c16e187a064d793663cc5d1ca901f0a7
matfij <mateusz.michal.fijak@gmail.com>
2025-02-03 21:04:10 +0100

    Simple game of life implementation

--- MauiBlazor/MauiSkiaSharp/Services/ParticleService.cs
+++ MauiBlazor/MauiSkiaSharp/Services/ParticleService.cs
@@ -1,28 +1,94 @@
-
 namespace MauiSkiaSharp.Services;
 
-public struct Particle(float x, float y, float r = 10)
+public struct Particle(float x, float y, bool alive = false)
 {
     public float X { get; set; } = x;
     public float Y { get; set; } = y;
-    public float R { get; set; } = r;
+    public bool Alive { get; set; } = alive;
 }
 
 public class ParticleService
 {
-    private readonly List<Particle> Particles = [];
+    private List<Particle> Particles = [];
+    private Random rnd = new Random();
 
     public ParticleService()
     {
-        Particles.Add(new Particle(10, 50));
-        Particles.Add(new Particle(100, 10));
-        Particles.Add(new Particle(250, 150, 25));
+        for (int x = 0; x < 100; x++)
+        {
+            for (int y = 0; y < 60; y++)
+            {
+                var isAlive = rnd.Next(0, 100) > 80;
+                Particles.Add(new Particle(x, y, isAlive));
+            }
+        }
+
     }
 
     public void AddParticle(Particle particle)
     {
-        Particles.Add(particle);
+        //for (int x = 0; x < 100; x++)
+        //{
+        //    for (int y = 0; y < 60; y++)
+        //    {
+        //        Particles.Add(new Particle(x, y, false));
+        //    }
+        //}
     }
 
     public IEnumerable<Particle> GetParticles => Particles;
+
+    public void Tick()
+    {
+        List<Particle> newParticles = [];
+
+        for (int x = 0; x < 100; x++)
+        {
+            for (int y = 0; y < 60; y++)
+            {
+                var aliveNeighbors = 0;
+
+                if (Particles.Find(p => p.X == x - 1 && p.Y == y - 1).Alive == true)
+                {
+                    aliveNeighbors += 1;
+                }
+                if (Particles.Find(p => p.X == x && p.Y == y - 1).Alive == true)
+                {
+                    aliveNeighbors += 1;
+                }
+                if (Particles.Find(p => p.X == x - 1 && p.Y == y).Alive == true)
+                {
+                    aliveNeighbors += 1;
+                }
+                if (Particles.Find(p => p.X == x + 1 && p.Y == y).Alive == true)
+                {
+                    aliveNeighbors += 1;
+                }
+                if (Particles.Find(p => p.X == x + 1 && p.Y == y + 1).Alive == true)
+                {
+                    aliveNeighbors += 1;
+                }
+                if (Particles.Find(p => p.X == x && p.Y == y + 1).Alive == true)
+                {
+                    aliveNeighbors += 1;
+                }
+                if (Particles.Find(p => p.X == x - 1 && p.Y == y + 1).Alive == true)
+                {
+                    aliveNeighbors += 1;
+                }
+                if (Particles.Find(p => p.X == x + 1 && p.Y == y - 1).Alive == true)
+                {
+                    aliveNeighbors += 1;
+                }
+
+                var oldParticle = Particles.Find(p => p.X == x && p.Y == y);
+                var isAlive =
+                    (oldParticle.Alive && (aliveNeighbors == 2 || aliveNeighbors == 3))
+                    || (!oldParticle.Alive && aliveNeighbors == 3);
+                newParticles.Add(new Particle(x, y, isAlive));
+            }
+        }
+
+        Particles = newParticles;
+    }
 }
