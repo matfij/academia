1d4209bcf87bfa149e9a3f07c39a5522b2a9aef9
matfij <mateusz.michal.fijak@gmail.com>
2025-07-07 21:01:11 +0200

    Introduce PDF reader

---
+++ csharp-masterclass/tickets-data-aggregator/Program.cs
@@ -0,0 +1,10 @@
+﻿using TicketsDataAggregator;
+
+
+ITicketsReader reader = new PdfTicketsReader();
+
+var content = reader.ReadAll();
+
+Console.WriteLine(content);
+
+Console.ReadLine();
---
+++ csharp-masterclass/tickets-data-aggregator/TicketsReader.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using UglyToad.PdfPig;
+
+namespace TicketsDataAggregator;
+
+interface ITicketsReader
+{
+    public string ReadAll();
+}
+
+internal class PdfTicketsReader : ITicketsReader
+{
+    private const string _format = "*.pdf";
+    private const string _path = @"C:\Users\mateu\Downloads\Tickets\Tickets";
+
+    public string ReadAll()
+    {
+        if (!Directory.Exists(_path))
+        {
+            throw new DirectoryNotFoundException();
+        }
+
+        var fileEntries = Directory.GetFiles(_path, _format);
+
+        var builder = new StringBuilder();
+
+        foreach (var entry in fileEntries)
+        {
+            using var document = PdfDocument.Open(entry);
+
+            foreach (var page in document.GetPages())
+            {
+                builder.AppendLine(page.Text);
+            }
+        }
+
+        return builder.ToString();
+    }
+}
