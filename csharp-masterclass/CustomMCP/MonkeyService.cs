3ddb0d9ccc70f841d6ede3ba3a4eeed36d626035
matfij <mateusz.michal.fijak@gmail.com>
2025-07-15 21:03:21 +0200

    Add monkey service

---
+++ csharp-masterclass/CustomMCP/MonkeyService.cs
@@ -0,0 +1,33 @@
+
+using System.Net.Http.Json;
+
+namespace CustomMCP;
+
+internal class MonkeyService
+{
+    private const string _baseUrl = "https://www.montemagno.com/monkeys.json";
+    HttpClient _httpClient = new();
+    List<Monkey>? _monkeys;
+
+    public async Task<List<Monkey>> GetAll()
+    {
+        if (_monkeys is not null)
+        {
+            return _monkeys;
+        }
+
+        var response = await _httpClient.GetAsync("");
+        if (response.IsSuccessStatusCode)
+        {
+            _monkeys = await response.Content.ReadFromJsonAsync<List<Monkey>>() ?? [];
+        }
+
+        return _monkeys ?? [];
+    }
+
+    public async Task<Monkey?> GetByName(string name)
+    {
+        var monkeys = await GetAll();
+        return monkeys.FirstOrDefault(m => m.Name == name);
+    }
+}
--- csharp-masterclass/CustomMCP/Program.cs
+++ csharp-masterclass/CustomMCP/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ModelContextProtocol.Server;
+using CustomMCP;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Logging.AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Trace);
@@ -11,6 +12,8 @@ builder.Services
     .WithStdioServerTransport()
     .WithToolsFromAssembly();
 
+builder.Services.AddSingleton<MonkeyService>();
+
 await builder.Build().RunAsync();
 
 [McpServerToolType]
@@ -18,4 +21,7 @@ public static class EchoTool
 {
     [McpServerTool, Description("Echoes message back to the client.")]
     public static string Echo(string message) => message;
+
+    [McpServerTool, Description("Echoes message length back to the client.")]
+    public static int EchoLength(string message) => message.Length;
 }
