bddd8c0d6f4aa2aabbe6fc4c1a2c357111b9b4c5
matfij <mateusz.michal.fijak@gmail.com>
2025-07-15 20:27:13 +0200

    Introduce MCP server

---
+++ csharp-masterclass/CustomMCP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Trace);
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes message back to the client.")]
+    public static string Echo(string message) => message;
+}
