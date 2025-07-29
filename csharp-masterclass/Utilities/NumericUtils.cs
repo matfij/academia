78bb493d34e79ecf3bb15cb7d38bf7792c08b468
matfij <mateusz.michal.fijak@gmail.com>
2025-07-29 20:34:07 +0200

    Setup test project

---
+++ csharp-masterclass/Utilities/NumericUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace Utilities;
+
+public class NumericUtils
+{
+    public static int GetEvenSumUpTo(int max)
+    {
+        var sum = 0;
+
+        for (int i = 0; i < max; i += 2)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
---
+++ csharp-masterclass/UtilitiesTests/NumericUtilsTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Utilities;
+
+namespace UtilitiesTests;
+
+[TestFixture]
+internal class NumericUtilsTest
+{
+    [Test]
+    public void Test_GetEvenSumUpTo()
+    {
+        var max = 0;
+
+        var sum = NumericUtils.GetEvenSumUpTo(max);
+
+        Assert.AreEqual(0, sum);
+    }
+}
