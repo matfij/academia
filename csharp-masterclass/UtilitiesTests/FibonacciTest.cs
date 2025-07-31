d9180e83f4a34bc896c751eaf4d5c3fce49fe8c7
matfij <mateusz.michal.fijak@gmail.com>
2025-07-31 20:11:55 +0200

    Unit test - fibonacci testing assignment

---
+++ csharp-masterclass/UtilitiesTests/FibonacciTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Utilities;
+
+namespace UtilitiesTests;
+
+[TestFixture]
+internal class FibonacciTest
+{
+    [Test]
+    public void Generate_ShallThrowException_ForNegativeNumber()
+    {
+        var exception = Assert.Throws<ArgumentException>(() => Fibonacci.Generate(-1));
+
+        Assert.AreEqual("n cannot be smaller than 0.", exception!.Message, "Invalid exception message");
+    }
+
+    [Test]
+    public void Generate_ShallThrowException_ForTooLargeNumber()
+    {
+        var exception = Assert.Throws<ArgumentException>(() => Fibonacci.Generate(47));
+
+        Assert.AreEqual(
+            "n cannot be larger than 46, as it will cause numeric overflow.", 
+            exception!.Message,
+            "Invalid exception message");
+    }
+
+    [TestCase(0, new int[] { })]
+    [TestCase(1, new int[] { 0 })]
+    [TestCase(2, new int[] { 0, 1 })]
+    public void Generate_ShallReturnArbitraryNumber_ForSpecialCases(int input, int[] expected)
+    {
+        var result = Fibonacci.Generate(input);
+
+        CollectionAssert.AreEqual(expected, result, $"Invalid result for {input}");
+    }
+
+    [TestCase(5, new int[] { 0, 1, 1, 2, 3 })]
+    [TestCase(10, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 })]
+    public void Generate_ShallReturnCorrectArray_ForValidNumber(int input, int[] expected)
+    {
+        var result = Fibonacci.Generate(input);
+
+        CollectionAssert.AreEqual(expected, result, $"Invalid result for {input}");
+    }
+}
