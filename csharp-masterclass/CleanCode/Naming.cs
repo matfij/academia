5fb4998ba7d9e938f2b6c88f58adcbdb31ebe7ff
matfij <mateusz.michal.fijak@gmail.com>
2025-08-12 20:54:37 +0200

    Setup clean code project

---
+++ csharp-masterclass/CleanCode/Naming.cs
@@ -0,0 +1,21 @@
+﻿namespace CleanCode;
+
+internal class Naming
+{
+    // prefer more descriptive names
+    // try keeping names short
+
+    public static string Reverse_Refactored(string input)
+    {
+        var letters = new char[input.Length];
+
+        var currentIndex = input.Length - 1;
+        foreach (var _char in input)
+        {
+            letters[currentIndex] = _char;
+            --currentIndex;
+        }
+
+        return new string(letters);
+    }
+}
---
+++ csharp-masterclass/CleanCode/Program.cs
@@ -0,0 +1,4 @@
+﻿// boy scout rule - leave code a bit better than it was before,
+// big refactor may not com soon
+
+// principle of least surprise - functions should do what they name suggests
