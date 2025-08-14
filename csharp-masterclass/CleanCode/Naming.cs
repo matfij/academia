f83083a3b7049cc172c476b2594b3675ea3f6725
matfij <mateusz.michal.fijak@gmail.com>
2025-08-14 20:40:06 +0200

    Clean code - naming donts

--- csharp-masterclass/CleanCode/Naming.cs
+++ csharp-masterclass/CleanCode/Naming.cs
@@ -5,6 +5,15 @@ internal class Naming
     // prefer more descriptive names
     // try keeping names short
 
+    // avoid:
+    // meaningless words: manager, handler, data, info
+    // over specific names - leaked implementation details
+    // Hungarian notation - variable type in name
+    // confusing names - too similar, too generic
+    // abbreviations - too many abbreviations in different contexts, hard to search
+    // abbreviations exceptions - well known exception ex, index i, j, math symbols
+    // context repetition - UserReader.ReadUser()
+
     public static string Reverse_Refactored(string input)
     {
         var letters = new char[input.Length];
@@ -18,4 +27,22 @@ internal class Naming
 
         return new string(letters);
     }
+
+    public static string BuildFilePathFrom_Refactored(DateTime dateTime, Extension extension)
+    {
+        var day = dateTime.Day;
+        var dayOfWeek = dateTime.DayOfWeek;
+        var month = dateTime.Month;
+        var year = dateTime.Year;
+
+        var extensionNormalized = extension.ToString().ToLower();
+
+        return $"{dayOfWeek}_{day}_{month}_{year}.{extensionNormalized}";
+    }
+}
+
+public enum Extension
+{
+    Txt,
+    Json
 }
