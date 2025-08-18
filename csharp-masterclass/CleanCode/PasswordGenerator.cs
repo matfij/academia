50f3b7e263836eb863f53c20b72d916dbd217df1
matfij <mateusz.michal.fijak@gmail.com>
2025-08-18 21:57:31 +0200

    Clean code - refactor password generator

---
+++ csharp-masterclass/CleanCode/PasswordGenerator.cs
@@ -0,0 +1,73 @@
+﻿namespace CleanCode;
+
+public interface IRandomGenerator
+{
+    public int GetNext(int range);
+    public int GetNextInRange(int min, int max);
+}
+
+public class QuickRandomGenerator : IRandomGenerator
+{
+    public int GetNext(int range) => Random.Shared.Next(range);
+    public int GetNextInRange(int min, int max) => Random.Shared.Next(min, max);
+}
+
+public class PasswordGenerator(IRandomGenerator randomGenerator)
+{
+    private readonly IRandomGenerator _randomGenerator = randomGenerator;
+
+    private const int _minLength = 1;
+    private const string _regularCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private const string _specialCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=";
+
+    public string Generate(int minLength, int maxLength)
+    {
+        var (isRangeValid, errorMessage) = ValidateLength(minLength, maxLength);
+        if (!isRangeValid)
+        {
+            throw new ArgumentOutOfRangeException(errorMessage);
+        }
+
+        var passwordLength = _randomGenerator.GetNextInRange(minLength, maxLength + 1);
+
+        return GenerateFrom(passwordLength, _regularCharacters);
+    }
+
+    public string GenerateSpecial(int minLength, int maxLength)
+    {
+        var (isRangeValid, errorMessage) = ValidateLength(minLength, maxLength);
+        if (!isRangeValid)
+        {
+            throw new ArgumentOutOfRangeException(errorMessage);
+        }
+
+        var passwordLength = _randomGenerator.GetNextInRange(minLength, maxLength + 1);
+
+        return GenerateFrom(passwordLength, _specialCharacters);
+    }
+
+    private (bool, string?) ValidateLength(int minLength, int maxLength)
+    {
+        if (minLength < _minLength)
+        {
+            return (false, $"{nameof(minLength)} must be greater than 0");
+        }
+        if (maxLength < minLength)
+        {
+            return (false, $"{nameof(minLength)} must be smaller than {nameof(maxLength)}");
+        }
+        else
+        {
+            return (true, null);
+        }
+    }
+    private string GenerateFrom(int length, string characters)
+    {
+        var passwordParts = Enumerable
+            .Repeat(characters, length)
+            .Select(characters => characters[_randomGenerator.GetNext(characters.Length)])
+            .ToArray();
+
+        return new string(passwordParts);
+    }
+}
--- csharp-masterclass/CleanCode/Program.cs
+++ csharp-masterclass/CleanCode/Program.cs
@@ -2,3 +2,15 @@
 // big refactor may not com soon
 
 // principle of least surprise - functions should do what they name suggests
+
+using CleanCode;
+
+var rand = new QuickRandomGenerator();
+var passwordGenerator = new PasswordGenerator(rand);
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(passwordGenerator.Generate(5, 10));
+    Console.WriteLine(passwordGenerator.GenerateSpecial(1, 20));
+}
+Console.ReadKey();
