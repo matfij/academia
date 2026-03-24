ccbc0f6bd105eb9e43a0cba07acf2bf45c225d91
matfij <mateusz.michal.fijak@gmail.com>
2026-03-24 20:16:13 +0100

    angular-21-starter: angular fundamentals

--- angular-21-starter/src/app/app.ts
+++ angular-21-starter/src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type OrderStatus = 'Preparing' | 'Cooking';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.sass'
+  styleUrl: './app.sass',
 })
 export class App {
-  protected readonly title = signal('angular-21-starter');
+  protected readonly title = signal('Healthy Recipe Box');
+  protected readonly subtitle = signal('Fresh seasonal meals in minutes');
+  protected readonly status = signal<OrderStatus>('Preparing');
+
+  protected startCooking() {
+    console.log('Cooking started!');
+    this.status.update(() => 'Cooking');
+  }
+
+  protected resetStatus() {
+    console.log('Cooking stopped!');
+    this.status.update(() => 'Preparing');
+  }
 }
